8d5c3f05a1db60b41a7b17f33bb153e4a2ee6c63
fadel <diopfadel06@gmail.com>
2024-07-21 14:41:23 +0000

    admin option validate

--- resources/js/Pages/DashboardAdmin.vue
+++ resources/js/Pages/DashboardAdmin.vue
@@ -5,7 +5,8 @@ import { Inertia } from '@inertiajs/inertia';
 
 const props = defineProps({
     immobiliers: Array,
-    voitures:Array,
+    voitures: Array,
+    users: Object
 });
 
 const data = reactive({
@@ -42,6 +43,7 @@ const navigateUpdateVenduImmobilier = (id) => {
     display: grid;
     grid-template-rows: 20% 80%;
 }
+
 .popup {
     position: fixed;
     top: 0;
@@ -53,6 +55,7 @@ const navigateUpdateVenduImmobilier = (id) => {
     justify-content: center;
     align-items: center;
 }
+
 .popup-content {
     background-color: white;
     padding: 20px;
@@ -73,49 +76,92 @@ const navigateUpdateVenduImmobilier = (id) => {
         <div class="custom-grid">
             <!-- All Articles -->
             <div class="flex flex-wrap justify-center mt-20 text-gray-800">
-                <div v-for="immobilier in immobiliers" :key="immobilier.id" @click="navigateToDetail(immobilier.id)" class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-2">
+                <div v-for="immobilier in immobiliers" :key="immobilier.id" @click="navigateToDetail(immobilier.id)"
+                    class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-2">
                     <div class="bg-secondaire rounded-2xl shadow-2xl relative mt-4">
                         <div class="w-full h-full">
-                            <img :src="'/storage/' + immobilier.image1" :alt="immobilier.imageAlt" class="w-full h-full object-fill rounded-2xl shadow-lg">
+                            <img :src="'/storage/' + immobilier.image1" :alt="immobilier.imageAlt"
+                                class="w-full h-full object-fill rounded-2xl shadow-lg">
                         </div>
-                        <img class="mb-3 w-[4rem] h-[4rem] rounded-full shadow-2xl absolute bottom-1/3 right-0" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Profile image">
+                        <img class="mb-3 w-[4rem] h-[4rem] rounded-full shadow-2xl absolute bottom-1/3 right-0"
+                            src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Profile image">
                         <div class="p-4">
-                            <h3 class="text-lg font-semibold text-white">{{ immobilier.nom }}</h3>
-                            <p class="text-gray-300">{{ immobilier.affaire }}</p>
-                            <p class="text-principal text-lg">{{ immobilier.prix }}<span class="text-principal text-opacity-60"> Fcfa</span></p>
+                            <h3 class="text-lg font-semibold text-white">{{ immobilier.nom }} : <span
+                                    class="text-green-300 text-xs">{{
+                                        immobilier.affaire
+                                    }}</span> </h3>
+
+                            <p class="text-principal text-lg">{{ immobilier.prix }}<span
+                                    class="text-principal text-opacity-60">
+                                    Fcfa</span></p>
                             <div class="flex items-center space-x-4 justify-between">
                                 <div class="flex flex-row space-x-1">
-                                    <!-- Placeholder for buttons if needed -->
+                                    <div v-for="us in user" :key="us.id">
+                                        <span class="text-principal">email :<span class="text-gray-300">{{ us.email
+                                                }}></span></span> <br>
+                                        <span class="text-principal">name:<span class="text-gray-300">{{ us.name
+                                                }}></span></span> <br>
+                                        <span class="text-principal">phone :<span class="text-gray-300">{{ us.phone
+                                                }}></span></span> <br>
+                                        <span class="text-principal">id : <span class="text-gray-300">{{ us.id
+                                                }}</span></span> <br>
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
             </div>
+
+
         </div>
         <!-- ---------------------------------------------------------------------------------------------------------- -->
         <div class="custom-grid">
 
             <div class="flex flex-wrap justify-center mt-20 text-gray-800">
-                <div v-for="voiture in voitures" :key="voiture.id" @click="navigateToDetailVoiture(voiture.id)" class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-2">
+                <div v-for="voiture in voitures" :key="voiture.id" @click="navigateToDetailVoiture(voiture.id)"
+                    class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-2">
                     <div class="bg-secondaire rounded-2xl shadow-2xl relative mt-4">
                         <div class="w-full h-full">
-                            <img :src="'/storage/' + voiture.image1" :alt="voiture.imageAlt" class="w-full h-full object-fill rounded-2xl shadow-lg">
+                            <img :src="'/storage/' + voiture.image1" :alt="voiture.imageAlt"
+                                class="w-full h-full object-fill rounded-2xl shadow-lg">
                         </div>
-                        <img class="mb-3 w-[4rem] h-[4rem] rounded-full shadow-2xl absolute bottom-1/3 right-0" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Profile image">
+                        <img class="mb-3 w-[4rem] h-[4rem] rounded-full shadow-2xl absolute bottom-1/3 right-0"
+                            src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Profile image">
                         <div class="p-4">
                             <h3 class="text-lg font-semibold text-white">{{ voiture.nom }}</h3>
                             <p class="text-gray-300">{{ voiture.affaire }}</p>
-                            <p class="text-principal text-lg">{{ voiture.prix }}<span class="text-principal text-opacity-60"> Fcfa</span></p>
-                            <div class="flex items-center space-x-4 justify-between">
-                                <div class="flex flex-row space-x-1">
+                            <p class="text-principal text-lg">{{ voiture.prix }}<span
+                                    class="text-principal text-opacity-60">
+                                    Fcfa</span></p>
+                            <span class="text-principal">name : <span class="text-gray-300">{{ voiture.user.name
+                                    }} <span class="text-principal">Id {{ voiture.user.id
+                                        }}</span></span></span> <br>
 
-                                </div>
-                            </div>
+                            <span class="text-principal">phone :<span class="text-gray-300">{{ voiture.user.phone
+                                    }}</span></span> <br>
+
+
+                            <span class="text-principal">email:<span class="text-gray-300">{{ voiture.user.email
+                                    }}</span></span> <br>
+
+
+                            <span class="text-principal">.boosted_at : <span class="text-gray-300">{{ voiture.boosted_at
+                                    }}</span></span> br
+
+                            <span class="text-principal">Duree : <span class="text-gray-300">{{ voiture.duration
+                                    }}</span></span>
+
                         </div>
+
                     </div>
                 </div>
             </div>
+
+
+
         </div>
 
         <!-- Popup -->
@@ -130,24 +176,25 @@ const navigateUpdateVenduImmobilier = (id) => {
 </template>
 
 <script>
-import {Head} from '@inertiajs/vue3'
+import { Head } from '@inertiajs/vue3'
 
 export default {
-  data() {
-    return {
-      sections: [
-        { id: 1, titre:"Mes rendez-vous" },
-      ],
-    }
-  },
-
-  methods: {
-    navigateToDetail(id) {
-      this.$inertia.visit(`/statusImmobilier/${id}`);
+    data() {
+        return {
+            sections: [
+                { id: 1, titre: "Mes rendez-vous" },
+            ],
+        }
     },
 
-    navigateToDetailVoiture(id) {
-      this.$inertia.visit(`/statusVehicule/${id}`);
-    }
-  },}
+    methods: {
+        navigateToDetail(id) {
+            this.$inertia.visit(`/statusImmobilier/${id}`);
+        },
+
+        navigateToDetailVoiture(id) {
+            this.$inertia.visit(`/statusVehicule/${id}`);
+        }
+    },
+}
 </script>
