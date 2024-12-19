ed936355854635af7ccacff0538ddf95d49fbe90
fadel <diopfadel06@gmail.com>
2024-12-19 23:17:33 +0000

    build app

--- resources/js/Layouts/Composant.vue
+++ resources/js/Layouts/Composant.vue
@@ -2,8 +2,8 @@
     <div class="bg-gradient-to-r from-principal via-secondaire to-principal text-white py-2">
         <div class="container mx-auto flex justify-between items-center px-4">
             <!-- Numéro de téléphone -->
-            <div class="flex items-center gap-1">
-                <i class="fab fa-whatsapp text-xs sm:text-sm md:text-base"></i>
+            <div class="flex items-center  gap-1">
+                <i class="fab fa-whatsapp text-green-400 text-xs sm:text-sm md:text-base"></i>
                 <a href="https://wa.me/221775266864?text=Exprimez%20vos%20besoins%20:%20trouvez%20votre%20futur%20logement,%20terrain%20ou%20véhicule%20en%20quelques%20clics."
                     target="_blank" rel="noopener noreferrer"
                     class="btn-primary relative text-xs sm:text-sm md:text-base">
